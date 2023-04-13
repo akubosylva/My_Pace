c2fc8902f92fb8792cc3356e7c5483e7e0ab8193
akubosylva <akubosylva@gmail.com>
2023-04-13 21:39:13 +0100

    Advance task_2

---
+++ sprint1/C-lang/0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - point of function entry
+ * Description: computes and prints the sum of all the 
+ * multiples of 3 or 5 below 1024 (excluded), followed by a new line
+ *
+ * Return: zero(0)
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+}
