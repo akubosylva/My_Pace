3a526ed6985af7f5288f375982d8f61b9df80695
akubosylva <akubosylva@gmail.com>
2023-04-14 03:13:21 +0100

    Multiply function

---
+++ sprint1/C-lang/0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiples two integers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
