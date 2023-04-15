34a60eb3daf99ab701944986e3353b2134888990
akubosylva <akubosylva@gmail.com>
2023-04-15 20:03:34 +0100

    Swap

---
+++ sprint1/C-lang/0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - a function that swaps the values of two integers
+ * @a: first pointer parameter
+ * @b: second pointer parameter
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
