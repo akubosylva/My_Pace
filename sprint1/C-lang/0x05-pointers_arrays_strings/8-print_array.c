d4bcc515f36661d8366672ee8e014e4a66e81759
akubosylva <akubosylva@gmail.com>
2023-04-16 01:28:57 +0100

    Copy String

---
+++ sprint1/C-lang/0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - a function that prints n elements of an array
+ * of integers, followed by a new line
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d, ", *(a + i));
+		else
+			printf("%d\n", *(a + i));
+	}
+}
