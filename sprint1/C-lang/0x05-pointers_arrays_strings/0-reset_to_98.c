3e9e13c257d8c9372e7177e9a93bcc924b3c4277
akubosylva <akubosylva@gmail.com>
2023-04-16 01:53:13 +0100

    Reset_to_98

--- sprint1/C-lang/0x05-pointers_arrays_strings/0-reset_to_98.c
+++ sprint1/C-lang/0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * reset_to_98 -  a function that takes a pointer to an int as 
+ * reset_to_98 -  a function that takes a pointer to an int as
  * parameter and updates the value it points to to 98
  * @n: integer parameter
  *
