dfadea98c0f65566d511eca5054ba37632aa8441
akubosylva <akubosylva@gmail.com>
2023-04-17 14:55:23 +0100

    Memset

---
+++ sprint1/C-lang/0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @s: string parameter
+ * @b: character parameter
+ * @n: number of byte
+ *
+ * Return: character pointer
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+	return (s);
+}
