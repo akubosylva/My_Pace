b560c7cb87ead94638f953a4fea38391861f4f58
akubosylva <akubosylva@gmail.com>
2023-04-18 23:26:25 +0100

    Print Reverse(recurion)

---
+++ sprint1/C-lang/0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev_recursion - a function that prints a string in reverse
+ * @s: string parameter
+ *
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
