ceab96b1bd4ccfbb9d05caf14e7e02714b94f302
akubosylva <akubosylva@gmail.com>
2023-04-18 23:55:21 +0100

    Power

---
+++ sprint1/C-lang/0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - a function that returns the value of 'x'
+ * raised to the power of 'y'
+ * @x: base integer
+ * @y: power integer
+ *
+ * Return: integer power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int power;
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (1);
+	power = x * _pow_recursion(x, (y - 1));
+
+	return (power);
+}
