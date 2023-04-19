147638a2ea8f9698f139c7910416965676a5a176
akubosylva <akubosylva@gmail.com>
2023-04-19 01:40:32 +0100

    Square_root

---
+++ sprint1/C-lang/0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * root - finds the natural root of a number
+ * @n: number
+ * @r: root integer
+ *
+ * Return: the root if found or -1
+ */
+
+int root(int n, int r)
+{
+	if (r * r > n)
+		return (-1);
+	if (r * r == n)
+		return (r);
+
+	return (root(n, (r + 1)));
+}
+
+/**
+ * _sqrt_recursion - a function that returns the
+ * natural square root of a number
+ * @n: integer parameter
+ *
+ * Return: square root of number
+ */
+
+int _sqrt_recursion(int n)
+{
+	int rt;
+
+	if (n < 1)
+		return (-1);
+	
+	rt = root(n, 0);
+	return (rt);
+}
