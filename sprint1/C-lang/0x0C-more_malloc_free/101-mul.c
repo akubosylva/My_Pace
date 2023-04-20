b46560340d8f2573ce5e9a0a45fb4a053e4b450d
akubosylva <akubosylva@gmail.com>
2023-04-20 03:27:56 +0100

    Mul_Integers

---
+++ sprint1/C-lang/0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - pointer of function entry
+ * Description: a program that multiplies two positive numbers
+ *
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Still working on the logic :) */
+}
