b1dd521d5dbeceffb4f16cb0c12e801b3e7cf447
akubosylva <akubosylva@gmail.com>
2023-04-20 15:54:47 +0100

    Create_dog_struct

--- sprint1/C-lang/0x0E-structures_typedef/dog.h
+++ sprint1/C-lang/0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - structure data type
+ * @name: name pointer
+ * @owner: owner pointer
+ * @age: age parameter
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
