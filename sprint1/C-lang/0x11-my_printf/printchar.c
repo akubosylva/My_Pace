91967b998cac0cd2b967da64cc427ecfa155267f
akubosylva <akubosylva@gmail.com>
2023-04-22 15:24:28 +0100

    Print_character

---
+++ sprint1/C-lang/0x11-my_printf/printchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_char - print character to stdout
+ * @ap: va_list parameter
+ * @count: word count
+ *
+ * Return: count
+ */
+
+int print_char(va_list ap, int count)
+{
+	int c;
+	c = va_arg(ap, int);
+	count += _putchar(c);
+
+	return (count);
+}
