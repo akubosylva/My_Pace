a15e27b6c16c0765ad80b65600512e9cb2e9e534
akubosylva <akubosylva@gmail.com>
2023-04-22 15:25:57 +0100

    get_func

---
+++ sprint1/C-lang/0x11-my_printf/get_func.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * get_func_print - a function pointer that get the function to print
+ * a format specifier
+ * @c: placeholder character
+ * @ap: va_list parameter
+ * @count: word_count parameter
+ *
+ * Return: count;
+ */
+
+int (*get_func_print(char c))(va_list, int)
+{
+	func funcArr[] = 
+	{
+		{'c', print_char},
+		{'s', print_string},
+		{'d', print_int},
+		{'i', print_int},
+		/*{'b', print_binary},*/
+		{'u', print_unsigned},
+		/*{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_Hex},
+		{'S', print_non_printable},
+		{'p', print_pointer},
+		{'l', print_long},
+		{'h', print_short},
+		{'r', print_reverse},
+		{'R', print_Rot13},*/
+		{'\0', NULL}
+	};
+
+	int i;
+
+	for (i = 0; funcArr[i].placeholder != '\0'; i++)
+	{
+		if (funcArr[i].placeholder == c)
+			return (funcArr[i].f);
+	}
+	return (NULL);
+}
