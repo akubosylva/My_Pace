45ddebed7de637039cfe41c7706dbb20f402b6ee
akubosylva <akubosylva@gmail.com>
2023-04-23 22:53:59 +0100

    Print_Before_Main()

---
+++ sprint1/C-lang/0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * before_main - print to stdout before execution os main function
+ * Note: made possible by prior declaration
+ * of "__attribute__ ((contructor))"
+ */
+
+void __attribute__ ((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I must bore my house upon my back!\n");
+}
