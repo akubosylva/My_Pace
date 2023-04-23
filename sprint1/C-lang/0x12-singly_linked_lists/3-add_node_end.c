dab85f364578ebd0d6ee936454f2ec1efbd2a500
akubosylva <akubosylva@gmail.com>
2023-04-23 22:32:59 +0100

    Add_Node_to_End

---
+++ sprint1/C-lang/0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_node_end - a function that adds a new node at the end of
+ * a list_t list
+ * @head: pointer to head pointer
+ * @str: string to add to the node end
+ *
+ * Return: address of the new element or NULL if failed
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int i, strL = 0;
+	list_t *newNode, *tmp;
+	
+	for (i = 0; *(str + i) != '\0'; i++)
+		strL++;
+	if (str == NULL)
+		return (NULL);
+	if (strdup(str) == NULL)
+		return (NULL);
+
+	newNode = malloc(sizeof(list_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->str = strdup(str);
+	newNode->len = strL;
+	newNode->next = NULL;
+
+	if (*head == NULL)
+		*head = newNode;
+	else
+	{
+		tmp = *head;
+
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = newNode;
+	}
+	return (newNode);
+}
