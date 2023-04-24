cff9ddc6f95d7ad0efe078e233af52cb696195e2
akubosylva <akubosylva@gmail.com>
2023-04-24 13:27:56 +0100

    Pop_listnode

---
+++ sprint1/C-lang/0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - a function that deletes the head node of
+ * a listint_t linked list, and returns the head node’s data (n)
+ * @head: pointer to first node of the list
+ *
+ * Return: head node's data(n) or 0 if list is empty
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	int data;
+
+	if (head == NULL)
+		return (0);
+	else
+	{
+		tmp = *head;
+		*head = tmp->next;
+		data = tmp->n;
+		free(tmp);
+	}
+	return (data);
+}
