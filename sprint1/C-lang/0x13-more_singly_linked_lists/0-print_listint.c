b38589f369c56fa115a310dbb96062d99d891821
akubosylva <akubosylva@gmail.com>
2023-04-24 01:14:18 +0100

    Print_List

---
+++ sprint1/C-lang/0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - a function that prints all the elements of a
+ * listint_t list
+ * @h: pointer to head of the node
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
