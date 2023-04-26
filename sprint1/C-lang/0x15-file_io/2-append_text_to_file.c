68dcb4b3e821b8f507533216dfc58909b66ac7ca
akubosylva <akubosylva@gmail.com>
2023-04-27 00:05:14 +0100

    Modify FIle

--- sprint1/C-lang/0x15-file_io/2-append_text_to_file.c
+++ sprint1/C-lang/0x15-file_io/2-append_text_to_file.c
@@ -25,8 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	wr = write(file, text_content, len);
 	if (wr == -1)
 	{
-		return (-1);
 		close(file);
+		return (-1);
 	}
 	close(file);
 	return (1);
