c7637e10f46a94825ea77f01d3623b4fecb855d5
akubosylva <akubosylva@gmail.com>
2023-04-26 18:09:27 +0100

    Append_Text_to_File

---
+++ sprint1/C-lang/0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - a function that appends
+ * text at the end of a file
+ * @filename: name of file to be opened for appending
+ * @text_content
+ *
+ * Return: 1 on success and -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, wr, len;
+
+	if (filename == NULL)
+		return (-1);
+	file = open(filename, O_WRONLY | O_EXCL | O_APPEND);
+	if (file == -1)
+		return (-1);
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+	if (text_content == NULL)
+		return (-1);
+	wr = write(file, text_content, len);
+	if (wr == -1)
+	{
+		return (-1);
+		close(file);
+	}
+	close(file);
+	return (1);
+}
