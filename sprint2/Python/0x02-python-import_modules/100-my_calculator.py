abc2ff276696ec86c9bfdbc7649fdcc2666b78a0
akubosylva <akubosylva@gmail.com>
2023-05-01 17:47:14 +0100

    My_Claculator

---
+++ sprint2/Python/0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+argc = len(argv)
+if argc != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+operator = argv[2]
+f = {"+": add, "-": sub, "/": div, "*": mul}
+if operator not in f:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+a = int(argv[1])
+b = int(argv[3])
+result = f[operator](a, b)
+
+if __name__ == "__main__":
+    print("{} {} {} = {}".format(a, operator, b, result))
