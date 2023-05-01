5b5721383006fc08b4068644b006359c209d26ab
akubosylva <akubosylva@gmail.com>
2023-05-01 01:49:43 +0100

    Print_reverse

---
+++ sprint2/Python/0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for number in reversed(range(97, 123)):
+    if number % 2 == 0:
+        pass
+    else:
+        number = number - 32
+    print("{:c}".format(number), end="")
