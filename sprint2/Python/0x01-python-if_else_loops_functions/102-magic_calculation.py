c2f80235fe2077487210bb70df3cb66f57585e50
akubosylva <akubosylva@gmail.com>
2023-05-02 01:34:59 +0100

    Magic_Calculation

--- sprint2/Python/0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ sprint2/Python/0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b, c):
-    # not yet understood
+    if (a < b):
+        return (c)
+    elif (c > b):
+        return (a + b)
+    else:
+        return ((a * b) - c)
+# uncomment the line of code below to see the bytecodes
+import dis
+print(dis.dis(magic_calculation))
