6222342facc549c0631b2f1e2eb68300c7f8ce52
akubosylva <akubosylva@gmail.com>
2023-05-02 01:54:31 +0100

    Magic_Calculation

--- sprint2/Python/0x02-python-import_modules/102-magic_calculation.py
+++ sprint2/Python/0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-    # I still dont under the concept of bytecode :(
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return (c)
+    else:
+        return (sub(a, b))
+# uncomment the line of code below to print bytecodes
+# import dis
+# print(dis.dis(magic_calculation))
