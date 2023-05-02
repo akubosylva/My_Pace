9301158563ff85d2371adc113177dd34bf698173
akubosylva <akubosylva@gmail.com>
2023-05-02 21:34:11 +0100

    Palindrome

--- sprint2/Python/0x03-python-data_structures/13-is_palindrome.c
+++ sprint2/Python/0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
  * is_palidrome - a function in C that checks if a singly
@@ -12,5 +10,32 @@
 
 int is_palindrome(listint_t **head)
 {
-	
+	listint_t *tmp = *head; 
+	unsigned int length = 0, i;
+	int data[1024]; /* create buffer to hold characters for comparison*/
+
+	if (head == NULL)
+		return (0);
+	if (*head == NULL)
+		return (1);
+	while (tmp) /*find length of list*/
+	{
+		tmp = tmp->next;
+		length += 1;
+	}
+	if (length == 1) /* a single character is a palidrome */
+		return (1);
+	tmp = *head;
+	while (tmp) /* copying to buffer */
+	{
+		data[i++] = tmp->n;
+		tmp = tmp->next;
+	}
+	for (i = 0; i <= (length / 2); i++)
+	{
+		/* comparing the characters from biginning to the middle */
+		if (data[i] != data[length - i - 1])
+			return (0);
+	}
+	return (1);
 }
