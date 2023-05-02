aed62a3dbf59059bb1714d4b1ff0af7f454e2c5b
akubosylva <akubosylva@gmail.com>
2023-05-02 23:34:37 +0100

    Palindrome

--- sprint2/Python/0x03-python-data_structures/13-is_palindrome.c
+++ sprint2/Python/0x03-python-data_structures/13-is_palindrome.c
@@ -10,30 +10,29 @@
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *tmp = *head; 
+	listint_t *tmp = *head;
 	unsigned int length = 0, i = 0;
-	int data[1024]; /* create buffer to hold characters for comparison*/
+	int data[1024]; /* create an array to hold data */
 
-	if (head == NULL)
+	if (head == NULL) /* if there is no list */
 		return (0);
-	if (*head == NULL)
-		return (1);
-	while (tmp) /*find length of list*/
+	while (tmp) /* find length of the list */
 	{
 		tmp = tmp->next;
-		length += 1;
+		length++;
 	}
-	if (length == 1) /* a single character is a palidrome */
+	if (length == 1) /* an empty list is considered a palindrome */
 		return (1);
 	tmp = *head;
-	while (tmp) /* copying to buffer */
+	while (tmp) /* fill array with data */
 	{
-		data[i++] = tmp->n;
+		data[i] = tmp->n;
 		tmp = tmp->next;
+		i++;
 	}
-	for (i = 0; i <= (length / 2); i++)
+	for (i = 0; i < (length / 2); i++)
 	{
-		/* comparing the characters from biginning to the middle */
+		/* if data differ return zero(0) */
 		if (data[i] != data[length - i - 1])
 			return (0);
 	}
