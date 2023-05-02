4d4255afc67b72004ad6843732cf890d3c4a2778
akubosylva <akubosylva@gmail.com>
2023-05-02 21:07:05 +0100

    Fix Bug

---
+++ sprint2/Python/0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * is_palidrome - a function in C that checks if a singly
+ * linked list is a palindrome
+ * @head: pointer to the first node pointer
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
+
+int is_palindrome(listint_t **head)
+{
+	
+}
