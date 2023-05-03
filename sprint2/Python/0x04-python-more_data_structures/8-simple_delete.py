79a0d10fb91268918ac9d375aa243e4fc3dff27d
akubosylva <akubosylva@gmail.com>
2023-05-04 00:39:34 +0100

    Delete_from_Dict

---
+++ sprint2/Python/0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# def simple_delete(a_dictionary, key=""):
+#    if a_dictionary is not None:
+#        if key in a_dictionary:
+#            del a_dictionary[key]
+#        return a_dictionary
+
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary and key in a_dictionary:
+        a_dictionary.pop(key)
+    return a_dictionary
