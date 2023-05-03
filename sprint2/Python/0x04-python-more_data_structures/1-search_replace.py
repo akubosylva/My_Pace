3be2cf748362a530375be0805249a25afa7fe63c
akubosylva <akubosylva@gmail.com>
2023-05-03 22:49:03 +0100

    Search_Replace

---
+++ sprint2/Python/0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    if my_list is not None:
+        return([replace if num == search else num for num in my_list])
+    return None
