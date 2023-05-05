6a7f9fda22846092eaf71fa989629a3800018ab6
akubosylva <akubosylva@gmail.com>
2023-05-05 01:09:03 +0100

    Roman_Numerals_to_Integer

--- sprint2/Python/0x04-python-more_data_structures/12-roman_to_int.py
+++ sprint2/Python/0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,36 @@
 #!/usr/bin/python3
 
+def value(r):
+    if (r == 'I'):
+        return 1
+    if (r == 'V'):
+        return 5
+    if (r == 'X'):
+        return 10
+    if (r == 'L'):
+        return 50
+    if (r == 'C'):
+        return 100
+    if (r == 'D'):
+        return 500
+    if (r == 'M'):
+        return 1000
+    return -1
+
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-    if roman_string == "":
-        return 0
-    num = 0
-    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    for i, j in zip(roman_string, roman_string[1:]):
-        if i not in dic.keys():
-            return 0
-        elif dic[i] >= dic[j]:
-            num += dic[i]
-        else:
-            num -= dic[i]
-    num += dic[roman_string[-1]]
-    return num
+   res = 0
+   i = 0
+   while (i < len(roman_string)):
+       s1 = value(roman_string[i])
+       if (i + 1 < len(roman_string)):
+           s2 = value(roman_string[i + 1])
+           if (s1 >= s2):
+               res = res + s1
+               i = i + 1
+           else:
+               res = res + s2 - s1
+               i = i + 2
+       else:
+           res = res + s1
+           i = i + 1
+   return res
