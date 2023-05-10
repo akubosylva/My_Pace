2a938ff9709e73e178348f57d65e7239fc553f2f
akubosylva <akubosylva@gmail.com>
2023-05-10 09:39:25 +0100

    Advance Task1

--- sprint2/Python/0x05-python-exceptions/100-safe_print_integer_err.py
+++ sprint2/Python/0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (TypeError, ValueError) as e:
-        stderr.write("Exception: {}\n".format(e))
+    except (TypeError, ValueError) as error:
+        stderr.write("Exception: {}\n".format(error))
         return False
