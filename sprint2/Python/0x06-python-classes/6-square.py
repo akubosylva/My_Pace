b60ef5dfb07c1124a5ba8076e17af7911b500648
akubosylva <akubosylva@gmail.com>
2023-05-11 02:34:45 +0100

    Private instance attribute

---
+++ sprint2/Python/0x06-python-classes/6-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size muse=t be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+    def area(self):
+        return (self.__size)**2
+
+    def my_print(self):
+        i = 0
+        if self.__size > 0:
+            while i < self.__position[1]:
+                print("")
+                i+= 1
+        if self.__size == 0:
+            print("")
+            return
+        
+        for i in range(self.__size):
+            for j  in range(self.__size + self.__position[0]):
+                if j < self.__position[0]:
+                    print(" ", end="")
+                else:
+                    print("#", end="")
+            print("")
+
