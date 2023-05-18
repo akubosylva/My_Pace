0ddd6034ebaa1f9a6d92e30b6da144a15f4137f1
akubosylva <akubosylva@gmail.com>
2023-05-18 13:44:38 +0100

    Modify File

--- sprint2/Python/0x08-python-more_classes/6-rectangle.py
+++ sprint2/Python/0x08-python-more_classes/6-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 class Rectangle:
-    number_of_instance = 0
+    number_of_instances = 0
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
