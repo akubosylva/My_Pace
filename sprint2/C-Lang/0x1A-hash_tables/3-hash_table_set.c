0d2fd660de05a15d378939efe2836de868f6c2ca
akubosylva <akubosylva@gmail.com>
2023-05-20 21:20:55 +0100

    Hash_table_set

---
+++ sprint2/C-Lang/0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,77 @@
+#include "hash_tables.h"
+
+/**
+ * create_and_add_node - a function that adds an element to the
+ * hash table
+ * @ht: head pointer
+ * @key: key
+ * @value: value
+ * @idx: index to insert
+ *
+ * Return: 1 if it succeeded, 0 otherwise
+ */
+
+int create_and_add_node(hash_table_t *ht, const char *key, const char *value, unsigned long int idx)
+{
+	hash_node_t *node = NULL;
+	char *k, *v;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	k = strdup(key);
+	if (!k)
+	{
+		free(node);
+		return (0);
+	}
+	v = strdup(value);
+	if (!v)
+	{
+		free(k);
+		free(node);
+		return (0);
+	}
+	node->key = k;
+	node->value = v;
+
+	if ((ht->array)[idx] == NULL)
+		node->next = NULL;
+	else
+		node->next = (ht->array)[idx];
+	(ht->array)[idx] = node;
+
+	return (1);
+}
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	/* get index */
+	/* if key already exits, update value and return */
+	/* else create node */
+	/* set ht idx ptr to node; else add node to front if collision */
+
+	unsigned long int idx;
+	hash_node_t *node = NULL;
+	char *v;
+
+	if (!ht || !(ht->array) || !key || strlen(key) == 0 || !value)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+
+	node = (ht->array)[idx];
+	while (node && (strcmp(key, node->key) != 0))
+		node = node->next;
+	if (node != NULL)
+	{
+		v = strdup(value);
+		if (!v)
+			return (0);
+		if (node->value)
+			free(node->value);
+		node->value = v;
+		return (1);
+	}
+
+	return (create_and_add_node(ht, key, value, idx));
+}
