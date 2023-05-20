2527d34ba3126d6705e68e3213a4f1bee3f82ee3
akubosylva <akubosylva@gmail.com>
2023-05-20 21:37:56 +0100

    Print_Hash_table

---
+++ sprint2/C-Lang/0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - a function that prints a hash table
+ * @ht: head pointer
+ *
+ * Return: void
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *node;
+	char *comma = "";
+
+	if (!ht || !ht->array)
+		return;
+	putchar('{');
+	while (idx < ht->size)
+	{
+		node = ((ht->array)[idx]);
+		while (node)
+		{
+			printf("%s'%s': '%s'", comma, node->key, node->value);
+			comma = ", ";
+			node = node->next;
+		}
+		idx++;
+	}
+	puts("}");
+}
